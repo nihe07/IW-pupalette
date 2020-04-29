8e081ce06b85de3056e98a4f6a2eaf8e41e81c63
nihe07 <46404033+nihe07@users.noreply.github.com>
2020-04-28 22:22:29 -0400

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -15,15 +15,6 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
 
-
-def db(request):
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
-
 def country(request):
     return render(request, "country.html")
 
